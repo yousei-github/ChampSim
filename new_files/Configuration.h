d36dee6ee3339020e669aa82e53e25928b092e62
Yousei <72794107+yousei-github@users.noreply.github.com>
2022-06-13 16:00:11 +0800

    Update Configuration.h

--- new_files/Configuration.h
+++ new_files/Configuration.h
@@ -20,26 +20,6 @@
 #include <stdint.h>
 
 /* defines */
-typedef enum
-{
-    read_default = 0,
-    read_end = 1,
-} ReadPINTraceType;
-
-typedef struct
-{
-    uint64_t PIN_trace_number;
-    uint64_t total_PIN_trace_number;
-    ReadPINTraceType read_PIN_trace_end;
-} PINTraceReadType;
-
-typedef struct
-{
-    FILE *trace_file;
-    char trace_string[1024];
-    uint64_t address;    // full_addr >> 6 = address (granularity: cache line or 64 Byte)
-    uint8_t access_type; // read access = 0, write access = 1
-} MemoryTraceFileType;
 
 /** @note
  * 631.deepsjeng_s-928B.champsimtrace.xz has 2000000000 PIN traces
